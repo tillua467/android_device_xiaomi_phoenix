f7921ce00fa571458c0b126f55716c07b8d711ef
Tillua467 <128046674+tillua467@users.noreply.github.com>
2024-11-05 01:26:27 +0600

    Delete vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,3 +0,0 @@
-echo 'Hello! we need to clone stuffs needed for your device, please wait tll process complete'
-
-git clone https://github.com/jaymistry258/device_xiaomi_phoenix-miuicamera.git vendor/xiaomi/phoenix-miuicamera --depth 1
